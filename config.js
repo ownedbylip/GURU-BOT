82baeab8604339adf9ac7e9fee697fc6806ad406
Nico Becker <132954042+ownedbylip@users.noreply.github.com>
2024-04-19 00:14:22 +0200

    config.js aktualisieren

--- config.js
+++ config.js
@@ -7,15 +7,15 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['491623949160', 'Nico', true],
+  ['4917666465572', 'Nico', true],
   [''] 
 ] //Number of owners
 
 //global.pairingNumber = "" //put your bot number here
 
-global.mods = ['491623949160'] 
-global.prems = ['491613949160']
-global.allowed = ['491623949160']
+global.mods = ['4917666465572'] 
+global.prems = ['4917666465572']
+global.allowed = ['4917666465572']
 global.keysZens = ['c2459db922', '37CC845916', '6fb0eff124']
 global.keysxxx = keysZens[Math.floor(keysZens.length * Math.random())]
 global.keysxteammm = ['29d4b59a4aa687ca', '5LTV57azwaid7dXfz5fzJu', 'cb15ed422c71a2fb', '5bd33b276d41d6b4', 'HIRO', 'kurrxd09', 'ebb6251cc00f9c63']
